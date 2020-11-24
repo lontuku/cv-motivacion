18cd0fa23b7ae0e63f77715dd5022df06b8bb021
Tomas K.T <tomas.konig.t@gmail.com>
2020-11-24 02:07:22 -0300

    Monitoreo con user

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
   //alert("Ready!");     
   // visita
+  function getRandomString(length) {
+    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var result = '';
+    for ( var i = 0; i < length; i++ ) {
+        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
+  }
+
+  var userName = ''
+
+  if (localStorage.getItem('userCv') == null){
+    userName = getRandomString(20)
+    localStorage.setItem('userCv', userName)
+  }
+  else{
+    userName = localStorage.getItem('userCv')
+  }
+  
+  //console.log(userName)
+
   const datum = new FormData();
-  datum.append('entry.731978348', 'visitado');
+  datum.append('entry.731978348', userName);
   fetch('https://docs.google.com/forms/u/0/d/e/1FAIpQLScTgB5u13efXrROfVWUcYyHXTksacCjRnMwtvmU9KKINP3jww/formResponse', {
     headers: {
       'Content-type': 'application/json'
@@ -11,6 +32,8 @@ document.addEventListener('DOMContentLoaded', function () {
     mode: 'no-cors',
     body: datum,
   });
+
+  
 
 });
 
